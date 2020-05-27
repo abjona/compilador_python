a98e15e23f830dd7c51ec09ed2bb5fb8917cca76
jonathan <abjonathan@hotmail.com>
2020-05-27 14:36:09 -0300

    add comentarios

--- comp/views.py
+++ comp/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from ply import lex
 
 
-def compilador(request):
-    return render(request, 'comp/compilador.html', {})
-
-
 def analiselexica(request):
 
     template_name = "comp/compilador.html"
@@ -22,7 +18,7 @@ def analiselexica(request):
 	   'print' : 'PRINT',
 	}
 
-	# tupla contendo os tokes
+	# TOKENS
     tokens =[
 	    'VAR','INT_VAL',
 	    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
@@ -31,7 +27,7 @@ def analiselexica(request):
 	    'AT','STRING','DOUBLE_VAL'
 	    ] + list(reserved.values())
 
-	# Tokens
+	# TOKENS - OP, CHAVES, PARENTESES
     t_AT      = r'\:'
     t_BEGIN   = r'\{'
     t_END     = r'\}'
@@ -43,26 +39,29 @@ def analiselexica(request):
     t_EQUALS  = r'='    
     t_LPAREN  = r'\('
     t_RPAREN  = r'\)'
-    # t_ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
+  
 
-
+    #VERIFICA VARIÁVEL
     def t_VAR(t):
         r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = reserved.get(t.value,'VAR')    # Check for reserved words
+        t.type = reserved.get(t.value,'VAR') 
         return t
     
+    #VERIFICA STRING
     def t_STRING(t):
         r'"([^"]+)"'
-        t.type = reserved.get(t.value,'STRING')
+        t.type = reserved.get(t.value,'STRING') 
         return t
     
+    #VERIFICA DOUBLE
     def t_DOUBLE_VAL(t):
-        r'[-+]?[0-9]+(\.([0-9]+)?([eE][-+]?[0-9]+)?|[eE][-+]?[0-9]+)'
+        r'[-+]?[0-9]+(\.([0-9]+)?([eE][-+]?[0-9]+)?|[eE][-+]?[0-9]+)' 
         t.type = reserved.get(t.value,'DOUBLE_VAL')
         return t
 
+    #VERIFICA VALOR INTEIRO
     def t_INT_VAL(t):
-        r'\d+'
+        r'\d+' 
         try:
             t.value = int(t.value)
         except ValueError:
@@ -70,23 +69,19 @@ def analiselexica(request):
             t.value = 0
         return t
 
-    # Ignored characters
+    #IGNORA CARACTER
     t_ignore = " \r"
-    # t_ignore = " \t"
-
 
     def t_COMMENT(t):
         r'\#.*'
         pass
-        # No return value. Token discarded
+        #NÃO RETORNA VALOR
 
     def t_newline(t):
         r'\n+'
         t.lexer.lineno += t.value.count("\n")
 
-    # Compute column. 
-    #     input is the input text string
-    #     token is a token instance
+    #ENCONTRA A COLUNA EM QUE O TOKEN FOI ENCONTRADO
     def find_column(input,token):
         last_cr = input.rfind('\n',0,token.lexpos)
         if last_cr < 0:
@@ -94,22 +89,20 @@ def analiselexica(request):
         column = (token.lexpos - last_cr) + 1
         return column
     lerror=[]
+
+    #VERIFICA ERROS
     def t_error(t):
-	    # print("Illegal character '%s'" % t.value[0])
 	    lr=(t.value[0],t.lineno,t.lexpos)
 	    lerror.append(lr)
 	    t.lexer.skip(1)
 	    
     lexer = lex.lex()
-	# lexer = lex.lex(optimize=1)
 
-
-	
-	# arq.write(data)
     lexer.input(str(codigo))
 	
-	
     llex=[]
+
+    #VERIFICA O TIPO DO TOKEN, VALOR, LINHA, COLUNA 
     while 1:
 	    tok = lex.token()
 	    if not tok: break
